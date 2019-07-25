96e25a4983790b2265010eebd420ae65c4036985
prashantgandhi27 <prashantgandhi7979@gmail.com>
2019-07-24 23:01:58 -0700

    Merge Sorted Array

---
+++ C++/merge_sorted_array.cpp
@@ -0,0 +1,42 @@
+/*
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+
+Note:
+
+The number of elements initialized in nums1 and nums2 are m and n respectively.
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+Example:
+
+Input:
+nums1 = [1,2,3,0,0,0], m = 3
+nums2 = [2,5,6],       n = 3
+
+Output: [1,2,2,3,5,6]
+*/
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+        unsigned short int index=0;
+        unsigned short int nums1_index=0;
+        for(int i=0; i<nums1.size(); i++){
+            if(index==n)
+                break;
+            else{
+                if(nums1[i]>=nums2[index]){
+                    nums1.insert(nums1.begin()+i,nums2[index]);
+                    nums1.pop_back();
+                    index++;
+                    i = -1;
+                }
+            }
+        }
+        nums1_index = nums1.size()-(nums2.size()-index);
+        while(index!=n){
+            nums1.insert(nums1.begin()+nums1_index,nums2[index]);
+            nums1.pop_back();
+            nums1_index++;
+            index++;
+        }
+    }
+};
